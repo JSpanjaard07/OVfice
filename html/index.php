3bd35cde59e40dd8d08910ab2a833607d6946259
JSpanjaard07 <157476395+JSpanjaard07@users.noreply.github.com>
2024-03-01 12:16:36 +0100

    Start

---
+++ html/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div class = 'rails'>
+        
+    
+        <div class = 'train' >
+            <div class = 'top' ></div>
+            <div class = 'front'>
+                <div class = 'bluepart'>
+                    <div class = 'window' >
+
+                    </div>
+                </div>
+            </div>
+            <div class='hoek-top'></div>
+            <div class='hoek'></div>
+
+        </div>
+        <div class = 'ovfice' >
+            <div class="ov-space"></div>
+            <p class = 'ov-text' >OVfice</p>
+            <div class='ov-space-two'></div>
+            <div class='login'>
+
+            </div>
+        </div>
+        <div class='planbackground'>
+            
+
+        </div>
+</div>        
+
+
+</body>
+</html>
